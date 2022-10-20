29fc85213824a5e10d1e1b484793845f2028c139
danila <danila_glazkov@mail.ru>
2022-10-21 00:43:28 +0400

    added moving car

--- server.js
+++ server.js
@@ -8,20 +8,80 @@ const port = 7000
 
 let players = []
 
+let tmp = 0;
+
 io.on('connection', (socket) => {
   console.log(`Client with id ${socket.id} connected`)
 
   socket.on('login', (login) => {
-    console.log('Login: ', login)
+    let loginExist = false;
       players.forEach(player => {
-        if(player.login === login)
+        if(player.login === login){
             socket.emit('login exist');
+            loginExist = true;
             return;
+        }
       });
-      players.push({
-        id: socket.id,
-        login: login
-      });
+      if(!loginExist){
+        players.push({
+          id: socket.id,
+          login: login,
+          x: 150+tmp,
+          y: 150,
+          velocity: 0,
+          rotate: 0
+        });
+        tmp+=100;
+        socket.emit("successful");
+      }
+  })
+
+  socket.on('key', (keys)=>{
+    if(keys===undefined)
+      return;
+    let player = players.find((player) => player.id === socket.id);
+    if(player===undefined)
+      return;
+    if(keys['a'] == true)
+    {
+      if(player.velocity > 0)
+        player.rotate -= 1.5;
+      if(player.velocity < 0)
+        player.rotate += 1.5;
+    }
+    if(keys['d'] == true)
+    {
+      if(player.velocity > 0)
+        player.rotate += 1.5;
+      if(player.velocity < 0)
+        player.rotate -= 1.5;
+    }
+    if(player.rotate > 360)
+      player.rotate = player.rotate-360;
+    if(player.rotate < -360)
+      player.rotate = player.rotate +360;
+
+    if(keys['w'] == true)
+      if(player.velocity<4)
+        player.velocity+=0.2;
+    if(keys['s'] == true){
+      if(player.velocity > 0)
+        player.velocity-=0.5;
+      if(player.velocity<=0)
+        if(player.velocity>-4)
+          player.velocity-=0.2;
+    }
+    if(keys['w']==false && keys['s']==false){
+        if(player.velocity >0.1)
+          player.velocity-=0.05;
+        else if(player.velocity <-0.1)
+          player.velocity +=0.05;
+        else 
+          player.velocity=0;
+    }
+    player.x+=player.velocity * Math.sin(player.rotate * Math.PI / 180);
+    player.y-=player.velocity * Math.cos(player.rotate * Math.PI / 180);
+    io.emit('redraw', players);
   })
 
   socket.on('disconnect', () => {
@@ -30,9 +90,11 @@ io.on('connection', (socket) => {
   })
 })
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname+'/client'))
 
-app.get('/', (req, res) => res.render('index'))
+app.get('/', (req, res) =>{
+  res.sendFile(__dirname+'/client/index.html')
+ })
 
 app.get('/players-count', (req, res) => {
   res.json({
