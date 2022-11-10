9ffab3d29caab16993549bac82273238f3d6617a
danila <danila_glazkov@mail.ru>
2022-11-11 03:17:16 +0400

    added color pick, drawing score table

--- client/script.js
+++ client/script.js
@@ -1,6 +1,11 @@
 let button = document.querySelector('#button');
+let colorSelect = document.querySelector('#color');
 let devError = document.querySelector('#error');
 let devLogin = document.querySelector('#devLogin');
+let devField = document.querySelector('#field');
+
+let width = 800;
+let height = 600;
 
 const widthCar = 30;
 const heightCar = 60;
@@ -11,38 +16,67 @@ const heightCoin = 25;
 const widthFire = 50;
 const heightFire = 50;
 
+let selectedColor = 'aqua';
+
+colorSelect.addEventListener('change', ()=>{
+  colorSelect.style.backgroundColor = colorSelect.value;
+  selectedColor = colorSelect.value;
+})
+
 button.onclick = () =>{
     let login = document.getElementById('login').value;
-    socket.emit('login', login);
+    socket.emit('login', {l: login, color: selectedColor});
     console.log('send');
-    socket.on('login exist', ()=>{
-        devError.innerHTML = 'login exist';
+    socket.on('bad login', (mes)=>{
+        devError.innerHTML = mes;
         return;
     })
     devError.innerHTML = '';
-    socket.on("successful", ()=>{
+    socket.on('successful', (size)=>{
         console.log("successful");
         devLogin.remove();
-        document.body.appendChild(canvas);
+        devField.appendChild(canvasScores);
+        devField.appendChild(canvas);
         setInterval(()=>{socket.emit('key', keys)}, 10);
     })
 }
 
-let socket = io()
-
-const background = new Image(800,600);
-background.src = 'assets/canvas_background.jpg';
+let socket = io();
 
 let canvas = document.createElement("canvas");
 let ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 600;
-canvas.style.display = "block";
-canvas.style.background = "white";
-canvas.style.margin = "0 auto"
+let canvasScores = document.createElement("canvas");
+let ctxScores = canvasScores.getContext("2d");
 
-const greenCar = new Image(5,5);
+
+const scoreBackground = new Image(width,height);
+scoreBackground.src = 'assets/score_background.jpg';
+canvasScores.width = width*0.4
+canvasScores.height = height
+canvasScores.style.background = 'white';
+
+const background = new Image(width,height);
+background.src = 'assets/canvas_background.jpg';
+canvas.width = width;
+canvas.height = height;
+canvas.style.background = 'white';
+canvas.style.margin = '0 auto';
+
+
+const aquaCar = new Image(30,60);
+aquaCar.src = 'assets/aqua_car.png';
+
+const greenCar = new Image(30,60);
 greenCar.src = 'assets/green_car.png';
+
+const purpleCar = new Image(30,60);
+purpleCar.src = 'assets/purple_car.png';
+
+const redCar = new Image(30,60);
+redCar.src = 'assets/red_car.png';
+
+const whiteCar = new Image(30,60);
+whiteCar.src = 'assets/white_car.png';
 
 const coinImage = new Image(5,5);
 coinImage.src = 'assets/coin.png';
@@ -59,7 +93,6 @@ document.addEventListener('keydown', (event) => {
   if(name ==='w' || name==='s' || name==='a' || name === 'd')
   {
     keys[name] = true;
-    //socket.emit('key', keys)
   }
 }, false);
 
@@ -70,32 +103,50 @@ document.addEventListener('keyup', (event) => {
   if(name ==='w' || name==='s' || name==='a' || name === 'd')
   {
     keys[name] = false;
-    //socket.emit('key', keys)
   }
 }, false);
 
 socket.on('redraw', (data)=>{
-  console.log('redraw');
   ctx.drawImage(background, 0, 0);
   const players = data[0]
+  let playerImage;
   players.forEach(player => {
-    console.log(player.x, player.y);
+    switch (player.color){
+      case 'aqua': playerImage = aquaCar;
+        break;
+      case 'green': playerImage = greenCar;
+        break;
+      case 'purple': playerImage = purpleCar;
+        break;
+      case 'red': playerImage = redCar;
+        break;
+      case 'white': playerImage = whiteCar;
+        break;
+    }
     ctx.save();
     ctx.translate(player.x, player.y);
     ctx.rotate(player.rotate * Math.PI/180);
-    //ctx.translate(-widthCar / 2, -heightCar / 2);
-    //ctx.drawImage(greenCar, player.x, player.y, widthCar, heightCar);
-    ctx.drawImage(greenCar, 0,0, widthCar, heightCar);
-    ctx.fillStyle = "rgb(255,255,255)";
+    ctx.translate(-widthCar / 2, -heightCar / 2);
+    ctx.drawImage(playerImage, 0, 0, widthCar, heightCar);
+    ctx.fillStyle = 'rgb(255,255,255)';
     ctx.rotate(-90* Math.PI/180);
     ctx.fillText(player.login, -40, -5, 60);
     ctx.restore();
   });
   const coin = data[1]
-  ctx.drawImage(coinImage, coin.x, coin.y, widthCoin, heightCoin);
-
+  ctx.drawImage(coinImage, coin.x - widthCoin / 2, coin.y-heightCoin / 2, widthCoin, heightCoin);
   const fires = data[2]
   fires.forEach(fire => {
-    ctx.drawImage(fireImage, fire.x, fire.y, widthFire, heightFire);
+    ctx.drawImage(fireImage, fire.x - widthFire / 2, fire.y - heightFire / 2, widthFire, heightFire);
+  });
+
+  ctxScores.drawImage(scoreBackground, 0, 0);
+  ctxScores.fillStyle = 'rgb(0,0,0)';
+  ctxScores.font = '25px serif';
+  ctxScores.fillText('player\t\t\tscore', 20, 30);
+  let current = 0;
+  players.sort((lhs, rhs)=>{return rhs.score - lhs.score;}).forEach(player => {
+    ctxScores.fillText(`${player.login}\t\t\t${player.score}`, 20, 60+current*30);
+    current+=1;
   });
 })
