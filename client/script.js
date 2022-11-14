884f79c8ba9a915cec7b8ad7ad105cb028974a7e
danila <danila_glazkov@mail.ru>
2022-11-14 17:47:38 +0400

    clean up

--- client/script.js
+++ client/script.js
@@ -8,7 +8,7 @@ let backButton = document.createElement('button');
 
 let width = 800;
 let height = 600;
-let scoresWidth = width;
+let scoresWidth = width*0.4;
 let scoresHeight = height;
 
 const widthCar = 30;
@@ -25,10 +25,57 @@ let selectedColor = 'aqua';
 let end = false;
 let ready = false;
 let inLobby = true;
+let firstStart = true;
 
 let login;
 
 let socket = io();
+
+const lobbyBackground = new Image(width,height);
+lobbyBackground.src = 'assets/lobby_background.jpg';
+
+let canvas = document.createElement('canvas');
+let ctx = canvas.getContext('2d');
+let canvasScores = document.createElement('canvas');
+let ctxScores = canvasScores.getContext('2d');
+
+
+const scoreBackground = new Image(width,height);
+scoreBackground.src = 'assets/score_background.jpg';
+canvasScores.width = scoresWidth;
+canvasScores.height = scoresHeight;
+canvasScores.style.background = 'white';
+
+const background = new Image(width,height);
+background.src = 'assets/canvas_background.jpg';
+canvas.width = width;
+canvas.height = height;
+canvas.style.background = 'white';
+canvas.style.margin = '0 auto';
+
+
+const aquaCar = new Image(30,60);
+aquaCar.src = 'assets/aqua_car.png';
+
+const greenCar = new Image(30,60);
+greenCar.src = 'assets/green_car.png';
+
+const purpleCar = new Image(30,60);
+purpleCar.src = 'assets/purple_car.png';
+
+const redCar = new Image(30,60);
+redCar.src = 'assets/red_car.png';
+
+const whiteCar = new Image(30,60);
+whiteCar.src = 'assets/white_car.png';
+
+const coinImage = new Image(5,5);
+coinImage.src = 'assets/coin.png';
+
+const fireImage = new Image(5,5);
+fireImage.src = 'assets/fire.png';
+
+let keys = {'w': false, 's':false, 'a':false, 'd':false};
 
 lobbyButton.onclick = ()=>{
   ready=!ready;
@@ -58,8 +105,6 @@ button.onclick = () =>{
     })
     devError.innerHTML = '';
 }
-
-let firstStart = true;
 
 socket.on('start', ()=>{
   end = false;
@@ -111,52 +156,6 @@ socket.on('updateLobby', (players)=>{
     i+=1;
   })
 })
-
-const lobbyBackground = new Image(width,height);
-lobbyBackground.src = 'assets/lobby_background.jpg';
-
-let canvas = document.createElement('canvas');
-let ctx = canvas.getContext('2d');
-let canvasScores = document.createElement('canvas');
-let ctxScores = canvasScores.getContext('2d');
-
-
-const scoreBackground = new Image(width,height);
-scoreBackground.src = 'assets/score_background.jpg';
-canvasScores.width = scoresWidth;
-canvasScores.height = scoresHeight;
-canvasScores.style.background = 'white';
-
-const background = new Image(width,height);
-background.src = 'assets/canvas_background.jpg';
-canvas.width = width;
-canvas.height = height;
-canvas.style.background = 'white';
-canvas.style.margin = '0 auto';
-
-
-const aquaCar = new Image(30,60);
-aquaCar.src = 'assets/aqua_car.png';
-
-const greenCar = new Image(30,60);
-greenCar.src = 'assets/green_car.png';
-
-const purpleCar = new Image(30,60);
-purpleCar.src = 'assets/purple_car.png';
-
-const redCar = new Image(30,60);
-redCar.src = 'assets/red_car.png';
-
-const whiteCar = new Image(30,60);
-whiteCar.src = 'assets/white_car.png';
-
-const coinImage = new Image(5,5);
-coinImage.src = 'assets/coin.png';
-
-const fireImage = new Image(5,5);
-fireImage.src = 'assets/fire.png';
-
-let keys = {'w': false, 's':false, 'a':false, 'd':false};
 
 document.addEventListener('keydown', (event) => {
   var name = event.key;
